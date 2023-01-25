93e2471d5fa6a28eb2c492b495a421c1c9c49bef
Artur-man <artur-man@hotmail.com>
2023-01-25 20:02:21 +0100

    configure pipelines

--- Kaiju_Pipeline/Pipeline.py
+++ Kaiju_Pipeline/Pipeline.py
@@ -19,6 +19,7 @@ rule cd_hit_dup:
                	read2 = lambda wildcards: config["fastq_dir"] + metadata.loc[wildcards.sample, "read2"]
 	params:
 		qsub=config["qsub_default"],
+		cd-hit-dup=config["cd-hit-dup"],
 		result_dir = config['result_dir'] 
 	output:
 		read1_nodup="fastq_files/{sample}_1_nodup.fastq",
@@ -32,7 +33,7 @@ rule cd_hit_dup:
 		mkdir -p fastq_files
 		zcat {input.read1} > fastq_files/{wildcards.sample}_1.fastq
 		zcat {input.read2} > fastq_files/{wildcards.sample}_2.fastq
-		cd-hit-dup -i fastq_files/{wildcards.sample}_1.fastq -i2 fastq_files/{wildcards.sample}_2.fastq -o {output.read1_nodup} -o2 {output.read2_nodup} 
+		{params.cd-hit-dup} -i fastq_files/{wildcards.sample}_1.fastq -i2 fastq_files/{wildcards.sample}_2.fastq -o {output.read1_nodup} -o2 {output.read2_nodup} 
 	
 		# Dedup summary
 		mkdir -p {params.result_dir}
@@ -58,8 +59,6 @@ rule cd_hit_dup_summary:
 
 # select fastq files, select files that exist
 def get_fastq_files(sample, readn):
-        # cur_file = os.path.isfile("fastq_files/" + sample + "_" + readn + "_nodup.fastq")
-
         if config["remove_duplicates"]:
                 return "fastq_files/" + sample + "_" + readn + "_nodup.fastq"
         else:
@@ -71,38 +70,40 @@ rule kaiju:
 		read2 = lambda wildcards: get_fastq_files(wildcards.sample, "2")
 	params:
 		qsub=config["qsub_kaiju"],
-		ref=config["reference"],
-		rname=config["rname"], 
-		kaiju_params=config["kaiju_params"]
+		kaiju = config['kaiju'],
+		kaiju_params=config["kaiju_params"],
+		reference_kaiju=config["reference_kaiju"],
+		kname=config["reference_kaiju_name"]
 	output:
 		kaiju_out = config['result_dir'] + "{sample}_kaiju.out"
 	shell:
 		'''
-		kaiju {params.kaiju_params} -t {params.ref}/nodes.dmp -f {params.ref}/kaiju_db_{params.rname}.fmi -i {input.read1} -j {input.read2} -o {output[0]}
+		{params.kaiju} {params.kaiju_params} -t {params.reference_kaiju}/nodes.dmp -f {params.reference_kaiju}/kaiju_db_{params.kname}.fmi -i {input.read1} -j {input.read2} -o {output[0]}
 		'''
 
 rule kaiju_table:
         input:
                 kaiju_out = config['result_dir'] + "{sample}_kaiju.out",
 	params:
-		qsub=config["qsub_default"],
-		ref=config["reference"],
-		result_dir = config['result_dir']
+	  qsub=config["qsub_default"],
+	  R = config['R'],
+	  result_dir = config['result_dir']
 	output:
 		table = config['result_dir'] + "{sample}_kaiju_table.csv"
 	shell:
 		'''
-		~/.conda/envs/r-base-4.0/bin/Rscript scripts/kaiju_table.R {input.kaiju_out} {output.table}
+		{params.R} scripts/kaiju_table.R {input.kaiju_out} {output.table}
 		'''
 
 rule kaiju_summary:
         input:
                 table = expand(config['result_dir'] + "{sample}_kaiju_table.csv", sample = sample_set)
 	params:
-		qsub=config["qsub_kaiju_summary"]
+		qsub=config["qsub_kaiju_summary"],
+		R = config['R']
 	output:
 		summary = config['result_dir'] + "kaiju_summary.out"
 	shell:
 		"""
-		~/.conda/envs/r-base-4.0/bin/Rscript scripts/kaiju_summary.R "{input.table}" {output.summary}
+		{params.R} scripts/kaiju_summary.R "{input.table}" {output.summary}
 		"""
